1519fbbed59aa8491c5006bb8d196203e13b77ef
Thivviya <53746818+Thivviya@users.noreply.github.com>
2019-08-19 21:21:22 +0530

    Update CalcGuvi.py

--- CalcGuvi.py
+++ CalcGuvi.py
@@ -1,21 +1,16 @@
 
 num1=int(input("Enter a frst no. "))
 num2=int(input("Enter a sec no. "))
-num3=int(input(print("The operations are 
-    1.Add
-    2.Sub
-    3.Mul
-    4.Div
-
-    Now,choose ur option by enter the no. "+num3))
-if num3==1:
+print("The operations are  1.Add 2.Sub 3.Mul 4.Div ..Now,choose ur option")
+num3=int(input(" "))
+if num3=='1':
   add(num1,num2)
-elif num3==2:
+elif num3=='2':
   sub(num1,num2)
-elif num3==3:
-  mul(num1,num2)
-elif num3==4:
-  div(num1,num2)
+elif num3=='3':
+  mul(num1*num2)
+elif num3=='4':
+  div(num1/num2)
 else:
   print("Enter a valid option!!")
 
@@ -29,9 +24,10 @@ def sub(num1,num2):
   print("The result for subtraction is "+c)
 
 def mul(num1,num2):
-  c=num1*num2)
-    print("The result for multiplication is "+c)
+  c=num1*num2
+  print("The result for multiplication is "+c)
 
 def div(num1,num2):
   c=num1/num2
-    print("The result for division is "+c)
+  print("The result for division is "+c)
+
