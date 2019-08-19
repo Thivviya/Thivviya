a8d75840238bd5b703d8eda3947f1fc26a99e5d6
Thivviya <53746818+Thivviya@users.noreply.github.com>
2019-08-19 19:43:13 +0530

    Create EvenGuvi.py

---
+++ EvenGuvi.py
@@ -0,0 +1,6 @@
+num1=int(input("Enter the starg value "))
+num2=int(input("Enter the endg value "))
+for i in range(num1,num2):
+  if i%2==0:
+    print("The even nos between the interval are ")
+    print(str(i))
